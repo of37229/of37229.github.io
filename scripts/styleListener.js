d985ee663b80f1360e46cea667ca4d0ec782d4f7
D <81710102+DGLight@users.noreply.github.com>
2026-01-21 15:03:30 -0600

    removed domain check that was causing issues, bad practice but nothing is secret or secure to hide in the first place here, so it's fine

--- scripts/styleListener.js
+++ scripts/styleListener.js
@@ -1,8 +1,5 @@
 window.addEventListener("message", (event) => {
-    //debugger;
-    if (event.orign == 'dglight.neocities.org'){
-        localStorage.setItem('style', event.data.styleSignal);
-        localStorage.setItem('shadershidden', false);
-        setTimeout(() => { location.reload(); }, 100);
-    }
+    localStorage.setItem('style', event.data.styleSignal);
+    localStorage.setItem('shadershidden', false);
+    setTimeout(() => { location.reload(); }, 100);
 });
