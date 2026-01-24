6a8510ffe2b6e6ca4da98dbccb51352dc064222e
D <81710102+DGLight@users.noreply.github.com>
2026-01-23 21:48:31 -0600

    removed the listener script- realized behavior should've been relegated to the styler itself since it's present on both parent and child pages. also added 10 more songs, removed 1 song

--- scripts/styleListener.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("message", (event) => {
-    localStorage.setItem('style', event.data.styleSignal);
-    localStorage.setItem('shadershidden', false);
-    setTimeout(() => { location.reload(); }, 100);
-});
